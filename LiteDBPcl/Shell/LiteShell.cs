1ce2f1b1fac4499dc8936ba3472e22b8238a7616
nohwnd <me@jakubjares.com>
2016-03-09 23:58:06 +0100

    Make LiteDB portable
    Based on the branch made by @lee-orr where he gets rid of emit. I took
    the code, moved it to PCL dll and did various fixes and omits to get it
    to build. Biggest of which is commenting out encryption. All changes are
    denoted by todo:

---
+++ LiteDBPcl/Shell/LiteShell.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using LiteDB.Shell.Commands;
+
+namespace LiteDB.Shell
+{
+    internal class LiteShell
+    {
+        private static readonly List<IShellCommand> _commands = new List<IShellCommand>();
+
+        static LiteShell()
+        {
+            //todo: sorry no dynamic lookup
+            //var type = typeof(IShellCommand);
+            //var types = AppDomain.CurrentDomain.GetAssemblies()
+            //    .SelectMany(s => s.GetTypes())
+            //    .Where(p => type.IsAssignableFrom(p) && p.IsClass);
+
+            //todo: list all
+            var types = new List<Type> {typeof (FileFind)};
+
+            foreach (var t in types)
+            {
+                var cmd = (IShellCommand) Activator.CreateInstance(t);
+                _commands.Add(cmd);
+            }
+        }
+
+        public BsonValue Run(DbEngine engine, string command)
+        {
+            if (string.IsNullOrEmpty(command)) return BsonValue.Null;
+
+            var s = new StringScanner(command);
+
+            foreach (var cmd in _commands)
+            {
+                if (cmd.IsCommand(s))
+                {
+                    return cmd.Execute(engine, s);
+                }
+            }
+
+            throw LiteException.InvalidCommand(command);
+        }
+    }
+}
